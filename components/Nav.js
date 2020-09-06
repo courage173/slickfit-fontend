98a123f54264cefd729ffbf66530ae60e2fe0049
courage173 <courageosemwengie@gmail.com>
2020-09-06 16:30:13 +0100

    created the nav page and styled it

--- components/Nav.js
+++ components/Nav.js
@@ -1,16 +1,25 @@
 import Link from 'next/link';
-
-const Nav = () => {
-    return (
-        <div>
+import NavStyles from './styles/NavStyles';
+const Nav = () => (
+    <NavStyles>
+        <>
+            <Link href='/items'>
+                <a>Items</a>
+            </Link>
             <Link href='/sell'>
                 <a>Sell!</a>
             </Link>
-            <Link href='/'>
-                <a>Home!</a>
+            <Link href='/signup'>
+                <a>Signup</a>
             </Link>
-        </div>
-    )
-}
+            <Link href='/orders'>
+                <a>Orders</a>
+            </Link>
+            <Link href='/me'>
+                <a>Account</a>
+            </Link>
+        </>
+    </NavStyles>
+)
 
 export default Nav
