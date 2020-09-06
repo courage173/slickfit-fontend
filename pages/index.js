e4ce766d34c78205c5110908641ea62cb5f7d64b
courage173 <courageosemwengie@gmail.com>
2020-09-06 13:32:39 +0100

    created the meta,shared page and header page

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+    return (
+        <div>
+            <h5>Home</h5>
+        </div>
+    )
+}
+
+export default index
---
+++ pages/sell.js
@@ -0,0 +1,9 @@
+const Sell = () => {
+    return (
+        <div>
+            <p>Selling page</p>
+        </div>
+    )
+}
+
+export default Sell
