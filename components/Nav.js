5c62a87c1bf778f01f1e6835686c5e68614f737e
courage173 <courageosemwengie@gmail.com>
2020-09-15 21:35:20 +0100

    created the user details component

--- components/Nav.js
+++ components/Nav.js
@@ -1,24 +1,31 @@
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
+import User from './User';
+
 const Nav = () => (
     <NavStyles>
-        <>
-            <Link href='/items'>
-                <a>Shop</a>
-            </Link>
-            <Link href='/sell'>
-                <a>Sell!</a>
-            </Link>
-            <Link href='/signup'>
-                <a>Signup</a>
-            </Link>
-            <Link href='/orders'>
-                <a>Orders</a>
-            </Link>
-            <Link href='/me'>
-                <a>Account</a>
-            </Link>
-        </>
+        <User>
+            {({ data: { me } }) => {
+                console.log(me)
+                return <p>User</p>
+            }}
+        </User>
+        <Link href='/items'>
+            <a>Shop</a>
+        </Link>
+        <Link href='/sell'>
+            <a>Sell!</a>
+        </Link>
+        <Link href='/signup'>
+            <a>Signup</a>
+        </Link>
+        <Link href='/orders'>
+            <a>Orders</a>
+        </Link>
+        <Link href='/me'>
+            <a>Account</a>
+        </Link>
+
     </NavStyles>
 )
 
