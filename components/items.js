51e32535963d0d0d04682b9be1289649b82f720d
courage173 <courageosemwengie@gmail.com>
2020-09-15 16:44:30 +0100

    created the signup

--- components/items.js
+++ components/items.js
@@ -34,7 +34,7 @@ class Items extends Component {
             <Center>
                 <Pagination page={this.props.page} />
                 <Query query={ALL_ITEMS_QUERY}
-                    // fetchPolicy='network-only' 
+                    // fetchPolicy='network-only'
                     variables={{
                         skip: this.props.page * perPage - perPage,
                     }}>
